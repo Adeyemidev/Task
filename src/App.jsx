0817d276c52cbbe84a92a233982115e5c2054e50
Adeyemi Ezekiel <adeyemiezekiel26@gmail.com>
2024-06-26 04:55:25 -0700

    task

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
 
-<div className='m-auto text-center px-10 h-screen grid place-content-center place-items-center '>
+<div className='m-auto text-center px-4 h-screen grid place-content-center place-items-center '>
 
 <div>
 <div className='px-20 pt-10 pb-10 bg-blue-700 rounded-t-2xl'>
@@ -20,13 +20,13 @@ function App() {
 <button className='text-blue-700 font-medium border py-1 px-3 rounded-xl flex items-center gap-2 m-auto bg-gray-50 hover:bg-gray-200'>Verified <MdVerified />
 </button>
 
-<div className='pt-10 flex flex-col gap-y-3 text-blue-700  font-medium max-w-32 m-auto'>
+<div className='pt-10 flex flex-col gap-y-3 text-blue-700  font-medium m-auto'>
 
-<button className='border py-2 px-1 bg-gray-50 hover:bg-gray-200'>Car Type</button>
-<button className='border py-2 px-1 bg-gray-50 hover:bg-gray-200' >Car Color</button>
-<button className='border py-2 px-1 bg-gray-50 hover:bg-gray-200'>Licence Plate</button>
-<button className='border py-2 px-1 bg-gray-50 hover:bg-gray-200'>Registered Park</button>
-<button className='border py-2 px-1 bg-gray-50 hover:bg-gray-200'>Share Button</button>
+<button >Car Type</button>
+<button >Car Color</button>
+<button>Licence Plate</button>
+<button>Registered Park</button>
+<button>Share Button</button>
 
 </div>
 </div> 
