22500461b56dc935702bbd4f439f03f6316906dd
Adeyemi Ezekiel <adeyemiezekiel26@gmail.com>
2024-06-26 04:47:45 -0700

    task

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,15 @@ function App() {
 
   return (
 
-<div className='m-auto text-center max-w-80 h-screen'>
+<div className='m-auto text-center px-10 h-screen grid place-content-center place-items-center '>
 
-<div className=' pt-10 pb-10 bg-blue-700 relative z-30 rounded-t-lg'>
+<div>
+<div className='px-20 pt-10 pb-10 bg-blue-700 rounded-t-2xl'>
   <p className='text-white text-center text-xl font-semibold pb-5'>Driver Profile</p> 
   <img className=' relative z-50 rounded-full border-4 h-28 mx-auto -mb-20' src={Jessica}  alt='Jessica Image' />
 
   </div>
-<div className='bg-white py-10 relative z.-30 rounded-b-lg'>
+<div className='bg-white py-10 relative z.-30 rounded-b-2xl px-20'>
 
   <p className='text-2xl font-semibold pt-6 pb-2 text-blue-700'>Ojo Taye Marcus </p>
 <button className='text-blue-700 font-medium border py-1 px-3 rounded-xl flex items-center gap-2 m-auto bg-gray-50 hover:bg-gray-200'>Verified <MdVerified />
@@ -29,9 +30,7 @@ function App() {
 
 </div>
 </div> 
-
-
-
+</div>
 
 
 </div>
